23d1a2047441a54df50ef7749f1ed7838a5ff509
FELIPE OLIVEIRA DE SOUZA <felipe.souza11@alunos.sc.senac.br>
2023-08-22 19:44:23 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { MainComponent } from './main/main.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MainComponent
+  },
+  {
+    path: 'categoria',
+    component:CategoriaComponent,
+    children:[
+      {
+        path:'',
+        component:CategoriaListarComponent
+      },
+      {
+        path:'form',
+        component:CategoriaFormComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    SidebarComponent,
+    CategoriaComponent,
+    CategoriaListarComponent,
+    CategoriaFormComponent
   ],
   imports: [
     BrowserModule,
